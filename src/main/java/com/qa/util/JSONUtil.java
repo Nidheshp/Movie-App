0d0f048860e688e72d4968c1dc08a74d141d0edd
Nidhesh <nidheshpillai@icloud.com>
2018-04-25 15:17:19 +0100

    JSon feature commit

--- src/main/java/com/qa/util/JSONUtil.java
+++ src/main/java/com/qa/util/JSONUtil.java
@@ -9,5 +9,14 @@ public class JSONUtil {
 	{
 		this.gson = new Gson();
 	}
-
+	
+	public String getJSONForObject(Object obj) 
+	{
+		return gson.toJson(obj);
+	}
+	
+	public <T> T getObjectForJSON(String jsonString, Class<T> clazz)
+	{
+		return gson.fromJson(jsonString, clazz);
+	}
 }
